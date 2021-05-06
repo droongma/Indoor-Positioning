3ddf716ddacdad0e2630a1eeaacc9017ddeb442c
droongma <agb2594@gmail.com>
2021-05-07 01:34:26 +0900

    Make several python programs which are used to make combined dataset
    파일설명.txt를 참고하면 각 파이썬 파일이 무슨 역할을 하는지 알 수 있음.

---
+++ ML(capstone)/raw_to_dataset(mobileData)/xls_to_csv.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+
+def xls_to_xlsx(pathname):
+    for (dirpath, dirnames, filenames) in os.walk(pathname):
+        print(dirpath)
+        current_dirname = dirpath.split('\\')[-1]
+        if "구역" in current_dirname: # if this directory contains excel file to be combined 
+            for filename in filenames:
+                fname, ext = os.path.splitext(filename)
+                if ext == '.xls': # if it is excel data file 
+                    file_abs = os.path.join(dirpath, filename)
+                    new_name = fname + ".csv" # new file name
+
+                    # convert xls to csv
+                    read_file = pd.read_excel(file_abs)
+                    read_file.to_csv(os.path.join(dirpath, new_name), index = None)
+            
+        print('\n---------------------')
+    
+    
+
+# xls to xlsx
+
+if __name__ == "__main__":
+    pass
